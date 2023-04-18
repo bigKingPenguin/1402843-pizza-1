c46250f42c631d9481c21c036e6a8500f5c25b07
Emiliia <emiliatoropova@yandex.ru>
2023-04-18 15:32:25 +0300

    Add VueRouter

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <div id="app">
-    <Index></Index>
+  <div>
+    <component
+      :is="layout"
+    >
+      <router-view/>
+    </component>
   </div>
 </template>
 
 <script>
-  import Index from '@/views/Index.vue';
   import '@/assets/scss/app.scss';
+  import {computed} from 'vue';
+  import {useRoute} from 'vue-router';
+  import appLayout from '@/layouts/AppLayout.vue';
 
   export default {
     name: 'App',
-    components: {Index},
+    components: {
+      appLayout: () => import('@/layouts/AppLayout.vue'),
+    },
+    setup() {
+      const route = useRoute();
+      const layout = computed(() => route.meta?.layout ? route.meta.layout : appLayout);
+      return {
+        layout,
+      };
+    },
   };
 </script>
 
--- src/frontend/src/layouts/AppLayout.vue
+++ src/frontend/src/layouts/AppLayout.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
     <Header></Header>
-    <Main></Main>
+    <main class="content">
+      <slot/>
+    </main>
   </div>
 </template>
 
 <script>
   import Header from '@/components/header/Header.vue';
-  import Main from '@/components/Main.vue';
 
   export default {
     name: 'MainLayout',
-    components: {Main, Header},
+    components: {Header},
   };
 </script>
 
